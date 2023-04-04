0c3c22ace3553af5a76a1d52818d547619732fe7
choro0121 <t.chay9271@gmail.com>
2023-04-04 12:13:08 +0900

    update nvim-lsp

--- .config/nvim/lua/plugins/nvim-lsp-installer.lua
+++ .config/nvim/lua/plugins/nvim-lsp-installer.lua
@@ -3,7 +3,7 @@ local lsp_installer = require('nvim-lsp-installer')
 lsp_installer.on_server_ready(function(server)
   local opts = {}
 
-  opts.capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  opts.capabilities = require('cmp_nvim_lsp').default_capabilities()
   vim.opt.completeopt = 'menu,menuone,noselect'
 
   opts.on_attach = function(client, bufnr)
